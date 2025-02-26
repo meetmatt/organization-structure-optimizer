b0b43158adc3622974e9b335f59f5a049487ee56
Iurii Golikov <iurii.golikov@gmail.com>
2025-02-26 11:30:03 +0100

    Initialized dummy maven app

---
+++ src/main/java/lu/golikov/oso/App.java
@@ -0,0 +1,13 @@
+package lu.golikov.oso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
